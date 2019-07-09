22e68475dd00f0ce95a93823833e17e2e878270d
Comelia <1511867031@qq.com>
2019-07-09 21:13:08 +0800

    配置路由 和 引入element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,13 @@
 // 配置路由
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  // 配置路由规则
+  routes: []
+})
+
+// 导出router
+export default router
