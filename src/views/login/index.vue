1fee4d209751eca6f6cd933ee910e7856655163a
Comelia <1511867031@qq.com>
2019-07-11 15:44:31 +0800

    登录组件-页面显示

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,39 @@
   <div class="login-container">
     <el-card class="box-card">
       <img src="../../assets/images/logo_index.png" alt="">
+      <!-- 表单 -->
+      <!-- 表单容器 -->
+      <el-form ref="loginForm" :model="loginForm">
+        <!-- 表单每一项容器 -->
+        <el-form-item>
+          <!-- 表单元素 -->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width: 240px"></el-input>
+          <el-button style="float: right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户此而已和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width: 100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
@@ -22,7 +48,7 @@ export default {
   background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
   .box-card {
     width: 400px;
-    height: 300px;
+    height: 320px;
     position: absolute;
     left: 50%;
     top: 50%;
