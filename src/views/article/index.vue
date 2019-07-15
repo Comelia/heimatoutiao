d12cc29ff744ab207b70a47c3ba84e9267effac0
Comelia <1511867031@qq.com>
2019-07-15 16:34:40 +0800

    内容管理-筛选列表显示

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -17,13 +17,11 @@
             <el-radio :label="4">审核失败</el-radio>
           </el-radio-group>
         </el-form-item>
-
         <el-form-item label="频道:">
           <el-select v-model="reqParams.channel_id" placeholder="请选择">
             <el-option v-for="item in channel" :key="item.id" :label="item.name" :value="item.id"></el-option>
           </el-select>
         </el-form-item>
-
         <el-form-item label="日期:">
           <el-date-picker
             v-model="dateValues"
@@ -33,7 +31,6 @@
             end-placeholder="结束日期"
           ></el-date-picker>
         </el-form-item>
-
         <el-form-item>
           <el-button type="primary">筛选</el-button>
         </el-form-item>
@@ -41,6 +38,47 @@
     </el-card>
     <!-- 筛选结果 -->
     <el-card class="box-card">
+      <div slot="header">
+        根据筛选条件同查询到 <b>0</b> 条结果:
+      </div>
+      <el-table :data="articles">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <!-- <img :src="scope.row.cover.images[0]" alt=""> -->
+            <!-- 图片组件 -->
+            <el-image :src="scope.row.cover.images[0]" style="width:100px;height:75px">
+              <!-- 当图片加载失败时显示占位图 -- 具名插槽 -->
+              <div slot="error">
+                <img src="../../assets/images/error.gif" width="100px" height="75px"  alt="" >
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态" >
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status === 1">待审核</el-tag>
+            <el-tag  v-if="scope.row.status === 2" type="success">审核通过</el-tag>
+            <el-tag  v-if="scope.row.status === 3" type="warning">审核失败</el-tag>
+            <el-tag  v-if="scope.row.status === 4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="">
+            <el-button icon="el-icon-edit" circle plain type="primary"></el-button>
+            <el-button icon="el-icon-delete" circle plain type="danger"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="box">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="1000">
+        </el-pagination>
+      </div>
     </el-card>
   </div>
 </template>
@@ -62,14 +100,43 @@ export default {
       // 频道的选项数组
       channel: [{ name: 'js', id: '1' }],
       // 日期数据
-      dateValues: []
+      dateValues: [],
+      // 文章列表数据
+      articles: []
     }
   },
   components: {
     // MyBread
+  },
+  created () {
+    // 获取频道数据
+    this.getChannel()
+    // 获取文章列表数据
+    this.getArticles()
+    console.log(this.articles)
+  },
+  methods: {
+    // 获取频道数据
+    async getChannel () {
+      // res  => { data: 响应内容 }  =>  { data: { data: { channels: {id,name},... } }}
+      // 解构赋值
+      const { data: { data } } = await this.$http.get('channels')
+      this.channel = data.channels
+    },
+    // 获取文章列表数据
+    async getArticles () {
+      // post 传参 post ('url',{参数对象})
+      // get 传参 get ('url?key=val&...')   get('url', {params: {参数对象}})
+      const { data: { data } } = await this.$http.get('articles', { params: this.reqParams })
+      this.articles = data.results
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.box{
+  text-align: center;
+  margin-top: 20px;
+}
 </style>
