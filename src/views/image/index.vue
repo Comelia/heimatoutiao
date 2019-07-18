f1977e77f1135dd755c366db0762fe3ac838adb8
Comelia <1511867031@qq.com>
2019-07-18 10:12:34 +0800

    素材管理-分页

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
     <div slot="header">
       <my-bread>素材管理</my-bread>
     </div>
@@ -14,19 +14,28 @@
     <!-- 图片列表 -->
     <ul class="pic-list">
       <li v-for="item in images" :key="item.id">
-        <img :src="item.url" alt="">
-        <div class="foot">
+        <img :src="item.url" alt />
+        <!-- v-show="!reqParams.collect"  当收藏时隐藏 下面图标 -->
+        <div class="foot" v-show="!reqParams.collect">
           <span class="el-icon-star-off" :class="{red:item.is_collected}"></span>
           <span class="el-icon-delete"></span>
         </div>
       </li>
     </ul>
     <!-- 分页器 -->
+    <!-- current-page 当前页数 -->
+    <!-- page-size 每页显示条数-->
+    <!-- current-change currentPage 改变时会触发-->
+    <!-- v-if="total>reqParams.per_page" 如果总条目数大于每页数量 显示分页器 -->
     <el-pagination
+      v-if="total>reqParams.per_page"
       background
+      :page-size="reqParams.per_page"
+      :current-page="reqParams.page"
+      @current-change="pager"
       layout="prev, pager, next"
-      :total="1000">
-    </el-pagination>
+      :total="total"
+    ></el-pagination>
   </el-card>
 </template>
 
@@ -42,19 +51,32 @@ export default {
         per_page: 10
       },
       // 素材列表
-      images: []
+      images: [],
+      // 总条数
+      total: 0,
+      loading: false
     }
   },
-  components: {},
   created () {
     // 获取素材列表数据
     this.getImages()
   },
   methods: {
+    // 分页
+    pager (newPage) {
+      this.reqParams.page = newPage
+      this.getImages()
+    },
     async getImages () {
-      const { data: { data } } = await this.$http.get('/user/images', { params: this.reqParams })
+      this.loading = true
+      const {
+        data: { data }
+      } = await this.$http.get('/user/images', { params: this.reqParams })
       // 获取数据成功
       this.images = data.results
+      // 设置总条数
+      this.total = data.total_count
+      this.loading = false
     },
     // 切换全部和收藏
     search () {
@@ -80,22 +102,22 @@ export default {
       height: 100%;
     }
     .foot {
-        position: absolute;
-        width: 100%;
-        height: 30px;
-        background: rgba(0,0,0,0.5);
-        color: #fff;
-        left: 0;
-        bottom: 0;
-        font-size: 18px;
-        text-align: center;
-        span {
-          margin: 0 20px;
-          &.red {
-            color: red
-          }
+      position: absolute;
+      width: 100%;
+      height: 30px;
+      background: rgba(0, 0, 0, 0.5);
+      color: #fff;
+      left: 0;
+      bottom: 0;
+      font-size: 18px;
+      text-align: center;
+      span {
+        margin: 0 20px;
+        &.red {
+          color: red;
         }
       }
+    }
   }
 }
 </style>
