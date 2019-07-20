7449eee55a555e6074abde3262028a3ec2868104
Comelia <1511867031@qq.com>
2019-07-20 20:31:50 +0800

    评论管理完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -16,13 +16,22 @@
         </el-table-column>
         <el-table-column label="操作" width="120px">
           <template slot-scope="scope">
-            <el-button v-if="!scope.row.comment_status" type="success" size="mini">打开评论</el-button>
-            <el-button v-else type="danger" size="mini">关闭评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" v-if="!scope.row.comment_status" type="success" size="mini">打开评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" v-else type="danger" size="mini">关闭评论</el-button>
           </template>
         </el-table-column>
       </el-table>
       <!-- 分页 -->
-      <el-pagination style="margin-top:20px" background layout="prev, pager, next" :total="1000"></el-pagination>
+      <el-pagination
+        style="margin-top:20px"
+        v-if="total>reqParams.per_page"
+        background
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+        layout="prev, pager, next"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -37,8 +46,10 @@ export default {
       reqParams: {
         page: 1,
         per_page: 20,
-        response_type: 'comments'
-      }
+        response_type: 'comment'
+      },
+      // 总条数
+      total: 0
     }
   },
   created () {
@@ -46,11 +57,36 @@ export default {
     this.getComments()
   },
   methods: {
+    // 改变文章评论状态
+    toggleStatus (row) {
+      const text1 = '您确定要打开文章的评论功能吗?'
+      const text2 = '关闭评论功能后用户将无法对该文章进行评论,您确定要关闭文章的评论功能吗?'
+      this.$confirm(row.comment_status ? text2 : text1, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(async () => {
+          const { data: { data } } = await this.$http.put(`comments/status?article_id=${row.id}`, {
+            allow_comment: !row.comment_status
+          })
+          this.$message.success('修改文章评论状态成功')
+          // 修改数据驱动试图更新
+          row.comment_status = data.allow_comment
+        }).catch(() => {})
+    },
+    // 分页
+    pager (newPage) {
+      this.reqParams.page = newPage
+      this.getComments()
+    },
     async getComments () {
       const { data: { data } } = await this.$http.get('articles', {
         params: this.reqParams
       })
       this.comments = data.results
+      // 总条数
+      this.total = data.total_count
     }
   }
 }
